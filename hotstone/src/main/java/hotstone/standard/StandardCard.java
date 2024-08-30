d5af4ce54c3d8a453453e5564747c3808f745f59
Momkey Bomkey <glutengut@gmail.com>
2024-08-30 11:29:15 +0200

    Implemented card class

---
+++ hotstone/src/main/java/hotstone/standard/StandardCard.java
@@ -0,0 +1,36 @@
+package hotstone.standard;
+
+import hotstone.framework.Player;
+import hotstone.framework.Card;
+
+public class StandardCard implements Card {
+    public String getName() {
+        return null;
+    }
+
+    public int getManaCost() {
+        return 0;
+    }
+
+  
+    public int getAttack() {
+        return 0;
+    }
+
+    public int getHealth() {
+        return 0;
+    }
+    
+    public boolean isActive() {
+        return false;
+    }
+
+    public Player getOwner() {
+        return null;
+    }
+
+    public String getEffectDescription() {
+        return null;
+    }
+
+}
